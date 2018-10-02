ca49807ef68367d182c250184e3b128991247eda
Unknown <domiaglialoro@gmail.com>
2018-10-01 21:33:15 -0700

    Replaced task IDs with strings. Improved task selection precision and code readability.
    Task IDs are now strings. This should make it easier to group similar tasks together. The random number generator now picks from a larger range of values.

--- public/index.php
+++ public/index.php
@@ -33,17 +33,17 @@ require __DIR__ . '/../src/middleware.php';
 // Run app
 
 $app->get('/celeste/', function (Request $request, Response $response, array $args) {
-    //generate semirandom seed based on a hash of current timestamp
+    //Generate semirandom seed based on a hash of current timestamp
     $date = date('YmdHis');
-    $seed = substr(md5($date),0,16);
+    $seed = substr(md5($date), 0, 16);
 
     return $response->withRedirect('/celeste/'.$seed);
 });
 
 $app->get('/celeste/{seed:\w+}', function (Request $request, Response $response, array $args) {
     $seed = $args['seed'];
-    //hash and substring input to get seed
-    $seed = substr(md5('74dPU18G'.$seed),0,16);
+    //Hash and substring input to get seed
+    $seed = substr(md5('74dPU18G'.$seed), 0, 16);
 
     $task_list = getTaskList($seed);
     $response = $this->renderer->render($response, 'index.phtml', ['task_list' => $task_list]);
@@ -52,14 +52,14 @@ $app->get('/celeste/{seed:\w+}', function (Request $request, Response $response,
 
 $app->run();
 
-//randomization logic -- create task list given seed
-function getTaskList($seed){
-    //retrieve task library and init vars 
+//Randomization logic. Creates a task list given a seed
+function getTaskList($seed) {
+    //Retrieve task library and initialize variables 
     $task_library = json_decode(file_get_contents('task_list.json'), true);
     $task_list = [];
     $removed_task_ids = [];
 
-    //init RNG
+    //Initialize RNG
     $rng = new SeedSpring($seed);
 	
     $chapter_names = [
@@ -72,21 +72,22 @@ function getTaskList($seed){
         "Summit"
     ];
 
+    //Get a task for each chapter
+    for($chapter = 1; $chapter <= 7; $chapter++){
+        //Generate possible tasks for the chapter
+        $merged_list = array_merge($task_library['general'], $task_library[$chapter]);
 
-    //get task per chapter
-    for($chapter = 1;$chapter <=7; $chapter++){
-        //gen possible tasks for the chaptr
-        $merged_list = array_merge($task_library['general'],$task_library[$chapter]);
-
-        //exclude strawberry-related tasks from chapter 6
+        //Exclude strawberry-related tasks from chapter 6
         if ($chapter != 6) {
             $merged_list = array_merge($merged_list,$task_library['strawberry']);
         }
 
         //Replace list weights with accumulated weight values
         $sum = 0;
+
         for ($i = 0; $i < count($merged_list); $i++) {
             $item = $merged_list[$i];
+
             if (array_key_exists('weight',$item)) {
                 $temp = $item['weight'];
             }
@@ -94,30 +95,34 @@ function getTaskList($seed){
             else {
                 $temp = 1;
             }
+
             $sum += $temp;
             $merged_list[$i]['weight'] = $sum;
         }
 
-        //obtain task avoiding repeating ones
+        //Obtain a task, avoiding repeating ones
         do {
-            $rand = $sum*($rng->getInt(0, 100)/100);
+            $rand = $sum * ($rng->getInt(0, 65536) / 65536);
             $rand_task = null;
+
             for($i = 0; $i < count($merged_list); $i++) {
-                if($rand <= $merged_list[$i]['weight']){
+                if($rand <= $merged_list[$i]['weight']) {
                     $rand_task = $merged_list[$i];
                     break;
                 }
             }
+
             if($rand_task == null){
-                $rand_task = $merged_list[count($merged_list)-1];
+                $rand_task = $merged_list[count($merged_list) - 1];
             }
-        } while((array_key_exists('task_id', $rand_task) && in_array($rand_task['task_id'],$removed_task_ids)));
+        } while((array_key_exists('task_id', $rand_task) && in_array($rand_task['task_id'], $removed_task_ids)));
+
         //Add task ID to used list, if present
         if (array_key_exists('task_id', $rand_task)) {
             $removed_task_ids[] = $rand_task['task_id'];
         }
 
-        $task_list[$chapter_names[$chapter -1]] = $rand_task['task_description'];
+        $task_list[$chapter_names[$chapter - 1]] = $rand_task['task_description'];
     }
     
     return $task_list;
